2c94e932ee42cd928a77926364272e7be70e302e
NouraBS <nooura_bs@hotmail.com>
2022-10-12 02:11:33 +0300

    add files

--- olympic.R
+++ olympic.R
@@ -15,7 +15,7 @@ library(dplyr)
 #import dataset
 olympic<- read.csv("athlete_events.csv")
 
-#take alook of our data
+#take a look of our data
 glimpse(olympic)
 skim(olympic)
 summary(olympic)
@@ -23,10 +23,10 @@ str(olympic)
 
 #clean our data
 clean_names(olympic)->olympic_df #clean names
+
 #modify type of data
 olympic_df$sex <- as.factor(olympic_df$sex)
 olympic_df$season <- as.factor(olympic_df$season)
-olympic_df$medal <- as.factor(olympic_df$medal)
 olympic_df$city <- as.factor(olympic_df$city)
 olympic_df$sport <- as.factor(olympic_df$sport)
 olympic_df$event <- as.factor(olympic_df$event)
@@ -35,24 +35,23 @@ olympic_df$noc <- as.factor(olympic_df$noc)
 
 summary(olympic_df)
 glimpse(olympic_df)
-
-sum(is.na(olympic_df)) #we have null value
 str(olympic_df)
 
+#To see if we have null value
+sum(is.na(olympic_df)) #we have null value
 
-# remove null value 
-olympic_df %>% group_by(medal) %>% count(medal)
 
-medalcount <- olympic_df %>% 
-  filter(!is.na(medal))%>%
-  group_by(medal) %>%
-  summarize(count=length(medal))
-medalcount
 
-# or we can replace it
-ans <- df %>% replace(.=="NULL", "Z") # replace with "Z"
+#remove NULL value in medal 
+#the NULL mean not winning any medals
+olympic_df %>% 
+  group_by(medal) %>% 
+  count(medal)
 
-muu<-olympic_df%>%
-  replace(.=="NULL",'not_win')
-muu
-muu %>% group_by(medal) %>% count(medal)
+olympic_df$medal[is.na(olympic_df$medal)]="Not win"
+table(olympic_df$medal)
+
+#modify type of data
+olympic_df$medal <- as.factor(olympic_df$medal)
+#take a look
+summary(olympic_df)
